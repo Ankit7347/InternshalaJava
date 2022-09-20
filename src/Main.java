33f654c8928ebaf62b786bb558f40db856542d21
Ankit Srivastava <ankitsri7347@gmail.com>
2022-09-20 16:10:52 +0530

    Class and object

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+	public static void main(String args[]){
+
+		rectangle rect =new rectangle();
+		rectangle breadth=new rectangle();
+		rect.length= 20;
+		rect.breadth=30;
+		rect.rectname="Susma";
+
+		rect.perimeter();
+		System.out.println(rect.rectname);
+		rect.area();
+
+
+
+	}
+}
---
+++ src/rectangle.java
@@ -0,0 +1,14 @@
+public class rectangle {
+	public int length;
+
+	public int breadth;
+	public String rectname;
+
+
+	public void area() {
+		System.out.println("The area of rectangle is " + length*breadth);
+	}
+	public void perimeter() {
+		System.out.println("The perimeter of rectangle is " +2*(length+breadth));
+	}
+}
